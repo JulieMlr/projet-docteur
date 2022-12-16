6ea5e55961f8bbbc3c678b806c625ce070156f3b
Julie Miler <julie.miler@ynov.com>
2022-12-16 12:24:10 +0100

    [ADD] bouton installer

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import Creation from './components/Creation';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Connexion from './components/Connexion';
 import Calendrier from './components/Calendrier';
+import { Button} from '@chakra-ui/react'
 
 function App() {
+  let deferredPrompt;
+  const installApp = document.getElementById('installApp');
+
+  if (installApp) {
+    installApp.addEventListener('click', async () => {
+      if (deferredPrompt !== null) {
+        deferredPrompt.prompt();
+        const { outcome } = await deferredPrompt.userChoice;
+        if (outcome === 'accepted') {
+          deferredPrompt = null;
+        }
+      }
+    })
+  }
 
   return (
     <div className="App">
+      <Button id="installApp" colorScheme='facebook' size='sm'>Installer</Button>
       <Router>
         <Routes>
           <Route path='/creation' element={<Creation />} />
